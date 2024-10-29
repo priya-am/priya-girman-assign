95612c2b0344a25a6150acfc15e600c0e04f967b
priya-am <142206598+priya-am@users.noreply.github.com>
2024-10-29 17:14:11 +0530

    girman

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <nav>            
+            <a href="http://girmantech.com">Website</a>
+            <a href="https://www.linkedin.com/company/girman-technologies">LinkedIn</a>
+            <a href="mailto:contact@girmantech.com">Contact</a>
+        </nav>
+    </header>
+);
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import SearchBox from '../components/SearchBox';
+
+const Home = () => (
+    <div>
+        <Header />
+        <SearchBox />
+    </div>
+);
+
+export default Home;
